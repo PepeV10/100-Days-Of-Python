3034ec3ca53d7af6e8ccbd41bf4c60f6896731b0
PepeV10 <47996667+PepeV10@users.noreply.github.com>
2020-12-23 22:49:13 -0600

    Add files via upload
    Day 2:
    Exercise 2 - BMI Calculator

--- day_2_exercise_2_bmi_calculator.py
+++ day_2_exercise_2_bmi_calculator.py
@@ -1,29 +1,15 @@
-# BMI Calculator
-# Instructions
-# Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+height = float(input("Please enter your height in meters - m: "))
+weight = float(input("Please enter your weight in kilograms - kg: "))
 
-# The BMI is a measure of some's weight taking into account their height. e.g. If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
+bmi = round (weight / height ** 2)
 
-# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
-
-
-
-# Warning you should convert the result to a whole number.
-
-# Example Input
-# weight = 80
-# height = 1.75
-# Example Output
-# 80 ÷ (1.75 x 1.75) = 26.122448979591837
-
-# 26
-
-# 🚨 Don't change the code below 👇
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
-
-bmi = float(weight) / float(height) ** 2
-print(int(bmi))
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
